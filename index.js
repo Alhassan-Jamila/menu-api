b334f50e79a2026daaeb99542d2ea08785291d56
Alhassan-Jamila <jamilaal865@gmail.com>
2022-04-19 15:12:29 +0000

    added user

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const express = require("express")
 const morgan =require("morgan")
 require("dotenv").config()
 const connectDB = require("./config/connectDB")
-const foodRoute = require("./routes/foodRoutes")
+const foodRoute = require("./routes/foodRoutes");
+const userRoute = require("./routes/userRoute");
+
+
+
+
 
 const app = express()
 
@@ -11,6 +16,7 @@ connectDB()
 app.use(express.json())
 app.use(morgan("dev"))
 app.use(foodRoute)
+app.use(userRoute)
 
 const PORT=process.env.PORT||8000
 
