7fe5e7a700938b941b2b2f6208da9b79bd0dabb5
Alhassan-Jamila <jamilaal865@gmail.com>
2022-04-25 14:25:34 +0000

    added some data

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const User = require("../models/userSchema");
 const bcrypt = require("bcryptjs");
 const validate = require("../config/validator");
+const {genrateToken, generateToken} =require("../utils/generateToken")
 
 //create a new user
 
@@ -10,17 +11,21 @@ const createUser = async (req, res)=> {
 
     if (valid) {
         const hashedPassword = await bcrypt.hash(valid.password, 10);
-        const user = new User({
+        const user = await User.create({
             username,
             email,
             password:hashedPassword,
         });
-        await user.save();
-
-        res.status(201).json({
-            message:"User created successful "
-        });
-    
+       
+        if (user) {
+            res.status(201).json({
+               username:user.name,
+                email:user.email,
+                id: user._id,
+                token:generateToken(user._id),
+            }); 
+        }
+        
     } else {
         res.status(400).json({
             message: "invalid data",
